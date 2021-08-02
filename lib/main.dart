4fa01bd7388289aefe3398e3b98a6bf89270dbc1
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-08-02 17:33:25 +0800

    Make provider and logging in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:indonesian_news_app/service/news_provider_list_service.dart';
+import 'package:logging/logging.dart';
 import 'package:indonesian_news_app/ui/home/home_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
+  _setupLogging();
   runApp(MyApp());
 }
 
+void _setupLogging(){
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((rec) {
+    print('${rec.level.name}: ${rec.time}: ${rec.message}');
+  });
+}
+
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomeScreen(),
+    return Provider(
+      create: (_) => NewsProviderListService.create(),
+      dispose: (_, NewsProviderListService service) => service.client.dispose(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomeScreen(),
+      )
     );
   }
 }
